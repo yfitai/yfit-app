4e61c0f791b2a69227b9649be2574eedc2c9a9bd
yfitai <support@yfitai.com>
2025-11-12 18:07:35 -0600

    Add 7 new form analysis exercises

--- src/components/FormAnalysisLive.jsx
+++ src/components/FormAnalysisLive.jsx
@@ -570,17 +570,18 @@ const FormAnalysisLive = () => {
     return feedback;
   };
 
-  const calculateAngle = (a, b, c) => {
-    // Calculate angle between three points
-    const radians = Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x);
-    let angle = Math.abs(radians * 180.0 / Math.PI);
-    
-    if (angle > 180.0) {
-      angle = 360 - angle;
-    }
-    
-    return angle;
-  };
+ function calculateAngle(a, b, c) {
+  // Calculate angle between three points
+  const radians = Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x);
+  let angle = Math.abs(radians * 180.0 / Math.PI);
+  
+  if (angle > 180.0) {
+    angle = 360 - angle;
+  }
+  
+  return angle;
+}
+
 
   const startAnalysis = async () => {
     if (!selectedExercise) {
