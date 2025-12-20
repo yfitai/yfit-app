468945731b1e6580c26a808f381e69bbde2d7fcd
yfitai <support@yfitai.com>
2025-12-20 13:19:44 -0600

    Fix closing div tags in signup password field

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -400,13 +400,11 @@ const [showSignupPassword, setShowSignupPassword] = useState(false)
       className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
     >
       {showSignupPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-    </button>
+       </button>
   </div>
+  <p className="text-xs text-gray-500">Minimum 6 characters</p>
 </div>
 
-              
-                    <p className="text-xs text-gray-500">Minimum 6 characters</p>
-                  </div>
                 </CardContent>
                 <CardFooter>
                   <Button 
