b6901dbfbdf3f4a02132429e35d4ecf7fccf4415
Don Campbell <campbelldon@hotmail.com>
2026-01-13 11:20:15 -0500

    Fix: Disable exhaustive-deps ESLint rule for date change useEffect

--- src/components/NutritionEnhanced.jsx
+++ src/components/NutritionEnhanced.jsx
@@ -65,6 +65,7 @@ export default function NutritionEnhanced({ user: propUser }) {
     if (user) {
       loadTodaysMeals(user.id, selectedDate)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedDate, user])
 
   const loadData = async () => {
